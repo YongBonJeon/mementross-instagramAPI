1d547b57ab04bbdc5f9dd8c63b23d6080007c0ab
YongBon Jeon <bon6143@gmail.com>
2024-04-10 23:17:58 +0900

    add posts/date api #4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from instagrapi import Client
+import datetime
 
 app = FastAPI()
 
@@ -28,17 +29,17 @@ async def getImages(tag: str, count: int):
 
     return images
 
-@app.get("/postings")
-async def getPostings(tag: str, count: int):
+@app.get("/posts")
+async def getPosts(tag: str, count: int):
     user_id = cl.user_id_from_username(tag)
     medias = cl.user_medias(user_id, count)
 
-    postings = {}
-    posting_idx = 1
+    posts = {}
+    post_idx = 1
     for media in medias:
-        posting = {}
-        posting["caption"] = media.caption_text
-        posting["date"] = media.taken_at
+        post = {}
+        post["caption"] = media.caption_text
+        post["date"] = media.taken_at
         images = {}
         if not media.resources:
             images[1] = media.thumbnail_url
@@ -47,8 +48,41 @@ async def getPostings(tag: str, count: int):
             for res in media.resources:
                 images[idx] = res.thumbnail_url
                 idx += 1
-        posting["images"] = images
-        postings[posting_idx] = posting
-        posting_idx += 1
+        post["images"] = images
+        posts[post_idx] = post
+        post_idx += 1
 
-    return postings
+    return posts
+
+@app.get("/posts/date")
+async def getPosts(tag: str, date: str, count: int):
+    user_id = cl.user_id_from_username(tag)
+    medias = cl.user_medias(user_id, 20)
+
+    criterion_date = datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    posts = {}
+    post_idx = 1
+    for media in medias:
+        post = {}
+        post_date = media.taken_at.replace(tzinfo=None)
+
+        if post_date > criterion_date:
+            continue
+        post["caption"] = media.caption_text
+        post["date"] = media.taken_at
+        images = {}
+        if not media.resources:
+            images[1] = media.thumbnail_url
+        else:
+            idx = 1
+            for res in media.resources:
+                images[idx] = res.thumbnail_url
+                idx += 1
+        post["images"] = images
+        posts[post_idx] = post
+        post_idx += 1
+        if post_idx > count:
+            break
+
+    return posts

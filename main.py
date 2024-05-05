ef2915cd8a2b0c9c68cccc3cdcb044369c7890ca
YongBon Jeon <bon6143@gmail.com>
2024-05-05 21:03:48 +0900

    gpt api 구현

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import json
+from typing import List, Optional
+
 from fastapi import FastAPI, Depends, HTTPException
 from instagrapi import Client
 import datetime
+import openai
 
 from sqlalchemy.orm import Session
 
 import crud
 from database import SessionLocal, engine
+from pydantic import BaseModel
+
+from const import openai_api_key
 
 app = FastAPI()
 
@@ -20,16 +26,55 @@ def get_db():
         db.close()
 
 
+class Item(BaseModel):
+    keywords: List[str]
+
+
+@app.post("/gpt")
+def generate(keywords: Item):
+    openai.api_key = openai_api_key()
+
+    query = ""
+    for keyword in keywords.keywords:
+        query += keyword + " "
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user",
+             "content": "#Context#"
+                        + "I want to write a diary in korean based on a Instagram post I uploaded"
+
+                        + "#Objective#"
+                        + "Act as a diary writer, that will write a diary based on the description of the photo and additional information provided by me."
+
+                        + "#Style#"
+                        + "It should be brief, its content only based on the description of the photo provided and additional information I provide. do not describe photo in the diary nor mention taking/uploading of the photo. diary should be written in past tense. "
+
+                        + "#Tone#"
+                        + "Do not exaggerate, write as if you are a blunt person, explaining to a friend. each phrases should be connected so that they form a long sentence, starting new sentence only if necessary. Do not end phrases with '습니다'or'한다'or'했다'or'었다'. if it does, convert to '어'"
+
+                        + "#Audience#"
+                        + "Only I will be the one to read it."
+
+                        + "#Response#"
+                        + "A short diary in korean."
+
+                        + "#description of the photo#"
+                        + query
+             }
+        ]
+    )
+
+
+    return response.choices[0].message
+
+
 @app.get("/members/{member_insta_username}")
 def read_member(member_insta_username: str, db: Session = Depends(get_db)):
     db_member = crud.get_member(db, member_insta_username=member_insta_username)
     if db_member is None:
         raise HTTPException(status_code=404, detail="Member not found")
     return db_member
-
-
-
-
 
 
 def update_member(db: Session, member_insta_username: str, member_insta_posting: str):
@@ -47,7 +92,8 @@ cl.login("mementross", "qwer1234!")
 
 @app.get("/images")
 async def getImages(tag: str, count: int):
-    user_id = cl.user_id_from_username(tag)
+    user_info = cl.user_info_by_username_v1(tag)
+    user_id = user_info.pk
     medias = cl.user_medias(user_id, count)
 
     images = {}
